d013b8c4dc6c85bae805d0d35c5bd022bc51073f
Gonzalo <gonzalorussi2502@gmail.com>
2026-05-30 21:56:35 -0300

    fix keys

--- src/firebase/firebaseAdmin.js
+++ src/firebase/firebaseAdmin.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 
 admin.initializeApp({
     credential: admin.credential.cert(
-        JSON.parse(process.env.FIREBASE_ACCOUNT_KEY)
+        JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
     ),
     projectId: "selecciona2-v2",
 });
