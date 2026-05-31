9b6da7f400d26ca9fa1629c9a7348568d8d98ab9
Gonzalo <gonzalorussi2502@gmail.com>
2026-05-30 21:17:16 -0300

    fix firebase

--- src/firebase/firebaseAdmin.js
+++ src/firebase/firebaseAdmin.js
@@ -1,6 +1,11 @@
 import admin from "firebase-admin";
 
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(
+        JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    ),
+    projectId: "selecciona2-v2",
+});
 
 const db = admin.firestore();
 
